5a619d0db4360e82fc1e4e56a9f8d7a4ba1591cb
immunarray <pitts.justin@gmail.com>
2020-05-01 08:01:50 -0400

    Removed DataConnector

---
+++ WO Grubb Notes Parser/WO Grubb Notes Parser/UserControl1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WO_Grubb_Notes_Parser
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
